a7c9fa05f12a40c072098c9e0036ad7a8dad531a
Robert McLeod <robert@autogrow.com>
2015-01-22 16:17:58 +1300

    made merge method convert a string use Dir[glob] and use Catalog.load

--- lib/tcl/msgcat.rb
+++ lib/tcl/msgcat.rb
@@ -17,13 +17,19 @@ module Tcl
     end
 
     def merge(root_file, translation_files=[])
+      if translation_files.is_a? String
+        translation_files = Dir[translation_files]
+      end
+
+      root = Tcl::Msgcat::Catalog.load(root_file)
+
       translation_files.each do |file|
         next if File.basename(file) == File.basename(root_file)
-        msgs = JSON.parse(File.read(file))
-        catalog = Tcl::Msgcat::Catalog.new(msgs)
+        catalog = Tcl::Msgcat::Catalog.load(file)
         catalog.merge!(root)
-
         File.open(file, "w") {|f| f.write catalog.to_json }
+
+        puts "Merged new translations into #{file}"
       end
     end
   end
